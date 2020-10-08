e5cda3cfe97eaa17ac0b96ac45b79d06ed6c8ff5
dilsonTorrefiel <68943296+dilsonTorrefiel@users.noreply.github.com>
2020-10-08 20:25:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function keyGenerator(){
 // Button animation
 function btnAnimate(Key){
   document.querySelector("#"+Key).classList.add("pressed");
-  setTimeout( function(){document.querySelector("#"+Key).classList.remove("pressed");}, 500);
+  setTimeout( function(){document.querySelector("#"+Key).classList.remove("pressed");}, 400);
 }
 
 // Play sound
